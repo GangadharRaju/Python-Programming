4428c31c73df03b80fe6c7b6fe4967f411f33316
Udayabhanu N P G Raju <unpgraju_cse@acoe.edu.in>
2021-05-24 16:58:28 +0530

    Add files via upload

---
+++ evenoddtest.py
@@ -0,0 +1,6 @@
+#EVEN OR ODD NUBER TEST
+num = int(input('Enter any number : ')) 
+if num % 2 == 0: 
+        print(f'The number {num} is a Even number') 
+else: 
+        print(f'The number {num} is a Odd number')
---
+++ ifelsetest.py
@@ -0,0 +1,11 @@
+A=10
+B=20
+if (A>B):
+      print("A is greater than B")
+      print("\n")
+      print("TURE BLOCK \n")
+else:
+     print("B is greater than A")
+     print("\n")
+     print("FALSE BLOCK \n")
+print("HELLO THIS IS END OF THE PROGRAM")
