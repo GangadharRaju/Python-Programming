e502885619ca4397dc69843b8b1c387484f8f7c4
Udayabhanu N P G Raju <unpgraju_cse@acoe.edu.in>
2021-05-31 10:52:12 +0530

    Create even_number_print.py

---
+++ even_number_print.py
@@ -0,0 +1,7 @@
+#print even numbers
+a=0
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n")
+for a in range(1,20):
+    if a%2==0:
+        print("the even number is:",a)
+print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
